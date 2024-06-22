19767e6c4d5864b62b6ffaf9265f26223a4ff573
Vitalii <vetali5700gmail.com>
2024-06-22 22:33:31 +0300

    add user login with token

--- planetarium_service/settings.py
+++ planetarium_service/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "drf_spectacular",
     "debug_toolbar",
+    "rest_framework.authtoken",
 ]
 
 MIDDLEWARE = [
--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from user.views import CreateUserView
+from user.views import CreateUserView, LoginUserView
 
 app_name = "user"
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
+    path("login/", LoginUserView.as_view(), name="get_token"),
 ]
