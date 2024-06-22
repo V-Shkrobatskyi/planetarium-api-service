e7f4c214c9c86d98052c8e8f41319ebfd635d10a
Vitalii <vetali5700gmail.com>
2024-06-22 23:13:49 +0300

    fix MEDIA_ROOT in settings.py

--- planetarium_service/settings.py
+++ planetarium_service/settings.py
@@ -125,8 +125,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+MEDIA_ROOT = BASE_DIR / "media"
+
 MEDIA_URL = "/media/"
-MEDIA_ROOT = "/files/media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
