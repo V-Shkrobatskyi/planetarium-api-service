12f26d51ed0bf19479d76c6008dd8d26dd36c6d5
Vitalii <vetali5700gmail.com>
2024-06-22 23:13:19 +0300

    fix path for save images in media

--- planetarium/models.py
+++ planetarium/models.py
@@ -31,7 +31,7 @@ def astronomy_show_image_path(instance, filename):
     _, extension = os.path.splitext(filename)
     filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
 
-    return os.path.join("uploads/movies/", filename)
+    return os.path.join("uploads/astronomy_shows/", filename)
 
 
 class AstronomyShow(models.Model):
