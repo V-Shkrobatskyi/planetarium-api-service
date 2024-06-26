dfc9b26ce185aa16b928715437b803ee9389f1f2
Vitalii <vetali5700gmail.com>
2024-06-26 22:51:09 +0300

    add python-dotenv and load environment variables from .env file

--- planetarium_service/settings.py
+++ planetarium_service/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ejsl#t%yd^pfn6zcx+*^lao4xd1@r)j^g_rdp_j%*clm_(qr84"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
